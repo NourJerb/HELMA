1a8c5ea6d3f413087b988176b2a1d86dd3744635
BAHAMLAOUHI <122161180+BAHAMLAOUHI@users.noreply.github.com>
2023-03-31 16:52:08 +0200

    Add files via upload

---
+++ assets/file.js
@@ -0,0 +1,16 @@
+import mail from "./mail.png"
+import icon from "./icon.png"
+import Instagram from "./Instagram.png"
+import Facebook from "./Facebook.png"
+import Twitter from "./Twitter.png"
+import location_icn from "./location_icn.png"
+import helmalogo from "./helmalogo.png"
+
+export {mail}
+export {Twitter}
+export{icon}
+export{Instagram}
+export{Facebook}
+export{location_icn}
+export{helmalogo}
+
