cb3f8d4bdfc0e21615e92a89c2cfa372694a7720
BAHAMLAOUHI <bahaeddine.mlaouhi@ensi-uma.tn>
2023-03-31 16:47:09 +0200

    item page.

--- src/pages/Item/Item.jsx
+++ src/pages/Item/Item.jsx
@@ -1,7 +1,46 @@
 import React from "react";
 import "./Item.css";
 function Item() {
-  return <div>Itemmmmmmmm</div>;
+  return  <div className="container-description">
+  <h1>sdgsdgsdgsdgsdgsdgsdg</h1>
+  <div className="presentation-description">
+     <div className="donnees-description">
+       <div>
+         <h4>Poids :</h4>
+         <span>dsgqsfsqfsqfqs</span>
+       </div>
+       <div>
+         <h4>Couleur de la pierre principale:</h4>
+         <span>kjbkhgkg</span>
+       </div>
+       <div>
+         <h4>Marque :</h4>
+         <span>sdsdfs</span>
+       </div>
+       <div>
+         <h4>Forme de la pierre principale:</h4>
+         <span>sdfsdfsf</span>
+       </div>
+       <div>
+         <h4>Matière:</h4>
+         <span>dsfsdfsdf</span>
+       </div>
+       <div>
+         <h4>Type:</h4>
+         <span>bague</span>
+       </div>
+     </div>
+     <div className="prod">
+     <img className="image-description" src="https://cdn.shopify.com/s/files/1/0557/9158/3406/products/cscplr2_f.jpg?v=1625149064" alt="wait" />
+     
+     <h4 className="prix-produit">500<span className="dt">DT</span></h4>
+     
+     </div>
+
+
+  </div>
+  <button className="button-description">Commander</button>
+</div>;
 }
 
 export default Item;
