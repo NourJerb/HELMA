9d8a39258ecbfc1f053e4ed0149f12251ae09955
maraaamaa <maramgabsi09@gmail.com>
2023-04-06 14:44:22 +0100

    Update Confirmer.jsx
    gg

--- src/pages/Confirmer/Confirmer.jsx
+++ src/pages/Confirmer/Confirmer.jsx
@@ -3,7 +3,14 @@ import "./Confirmer.css";
 import image3 from "../assets/image3.png"
 
 function Confirmer() {
-  return <div>Confirmer</div>;
+  return <div>
+    
+    <img src={image3}/>
+    <p className="first-paragraph"> Votre lettre est reçue avec succés.</p><br/>
+    <p className="second-paragraph"> Notre manager vous contactera dans les plus brefs délais</p><br/>
+    <input type="button" value="Retourner" className="button"></input>
+     
+  </div>;
 }
 
 export default Confirmer;
