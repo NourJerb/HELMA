f7ef799e97c43815dae2eb13e7cd944234ecb756
NourJerb <nour.jerbi@ensi-uma.tn>
2023-06-24 12:48:40 +0100

    merge maram

--- src/App.js
+++ src/App.js
@@ -2,10 +2,13 @@ import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home/Home";
 import React from 'react';
 import Contacts from "./pages/Contacts/Contacts";
-import Presentation from "./pages/Item/Item";
-import Services from "./pages/Commander/Commander";
-import NavBar from "./components/Navbar";
-import Footer from "./components/Footer";
+
+import NavBar from "./components/NavBar.js";
+import Shop from "./pages/Shop/Shop";
+import Item from "./pages/Item/Item";
+import Commander from "./pages/Commander/Commander.jsx";
+import Confirmer from "./pages/Confirmer/Confirmer.jsx";
+import HelmaFooter from "./Footer/HelmaFooter";
 
 
 
@@ -14,20 +17,24 @@ import Categories from "./components/Categories.js";
 function App() {
   return (
     <>
-      <NavBar />
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/contacts" element={<Contacts />} />
-          <Route path="/shop" element={<Shop />} />
-          <Route path="/item" element={<Item/>} />
-          <Route path="/commander" element={<Commander/>} />
-          <Route path="/confirmer" element={<Confirmer/>} />
-          <Route path="/home" element={<Home/>} />
-          <Route path="*" element={<div>404 not found</div>} />
-        </Routes>
-      </BrowserRouter>
-      <Footer />
+      <div className="App1">
+        <NavBar className="navigationBar"></NavBar>
+
+        <Categories></Categories>
+      </div>
+      
+
+      <Routes>
+        <Route path="/contacts" element={<Contacts />} />
+        <Route path="/shop/*" element={<Shop />} />
+        <Route path="/item" element={<Item />} />
+        <Route path="/commander" element={<Commander />} />
+        <Route path="/confirmer" element={<Confirmer />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="*" element={<div>404 not found</div>} />
+      </Routes>
+      <HelmaFooter/>
+      
     </>
   );
 }
