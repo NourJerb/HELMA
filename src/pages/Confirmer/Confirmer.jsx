9405417156dbf269fac486836809f069bc70fef3
Abdel Majid Maalej <103139965+AbdelMajidMaalej@users.noreply.github.com>
2023-06-24 15:49:26 +0100

    confirmer

--- src/pages/Confirmer/Confirmer.jsx
+++ src/pages/Confirmer/Confirmer.jsx
@@ -4,13 +4,13 @@ import image3 from "../../assets/image3.png"
 
 function Confirmer() {
 
-  return <div className={styles.big} style={{border:"solid",width:"80%",marginLeft:"10%",alignSelf: "center",marginTop:"3.5%",marginBottom:"3.5%",borderRadius:"3vh",border:"1px solid #707070"}}>
+  return <div className={styles.big} style={{border:"solid",width:"80%",marginLeft:"10%",alignSelf: "center",marginTop:"2.8%",marginBottom:"2.8%",borderRadius:"3vh",border:"1px solid #707070"}}>
     <div  className={styles.ct}>
       <div className={styles.imagecontainer}>
     <img className={styles.image} src={image3}/>
     </div>
     <div className={styles.paragraph}>
-    <p className={styles.first_paragraph}> Votre lettre est reçue avec succés.</p><br/>
+    <p className={styles.first_paragraph}> Votre lettre est reçue avec succés</p>
     <p className={styles.second_paragraph}> Notre manager vous contactera dans les plus brefs délais</p>
     <input type="button" value="Retourner" className={styles.buuton}></input>
     </div>
