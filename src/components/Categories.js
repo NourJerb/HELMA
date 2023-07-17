56ba6ee53a88bd8de9a2330d571c7b14b81dc177
selimketata <121767998+selimketata@users.noreply.github.com>
2023-07-17 10:09:43 +0100

    categories

--- src/components/Categories.js
+++ src/components/Categories.js
@@ -4,50 +4,52 @@ import styles from './Categories.module.css';
 import iconfleche from './../assets/iconfleche.png'
 import { useNavigate } from 'react-router-dom';
 import { products } from '../data/data.js';
-function Categories(){
-    const navigate=useNavigate()
-    const [brands, setBrands] = useState([
-        { id: 1, name: 'Necklaces' },
-        { id: 2, name: 'Earnings' },
-        { id: 3, name: 'Bracelets' },
-        { id: 4, name: 'Rings' },
-        { id: 1, name: 'Brooches' },
-        { id: 2, name: 'Anklets' },
-        { id: 3, name: 'Belly chains' },
-        { id: 4, name: 'Nose rings' },
-        { id: 4, name: 'Headbands' },
-        { id: 1, name: 'Hairbands' },
-        { id: 2, name: 'Charms' },
-        { id: 3, name: 'Chokers' },
-        { id: 4, name: 'Pendants' },
-        { id: 4, name: 'Cufflinks' },
-        { id: 4, name: 'Tiara'},
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faXmark } from '@fortawesome/free-solid-svg-icons';
+function Categories() {
+  const navigate = useNavigate()
+  const [brands, setBrands] = useState([
+    { id: 1, name: 'Necklaces' },
+    { id: 2, name: 'Earnings' },
+    { id: 3, name: 'Bracelets' },
+    { id: 4, name: 'Rings' },
+    { id: 1, name: 'Brooches' },
+    { id: 2, name: 'Anklets' },
+    { id: 3, name: 'Belly chains' },
+    { id: 4, name: 'Nose rings' },
+    { id: 4, name: 'Headbands' },
+    { id: 1, name: 'Hairbands' },
+    { id: 2, name: 'Charms' },
+    { id: 3, name: 'Chokers' },
+    { id: 4, name: 'Pendants' },
+    { id: 4, name: 'Cufflinks' },
+    { id: 4, name: 'Tiara' },
 
-        { id: 4, name: 'Nose studs' },
-        { id: 4, name: 'Toe rings' },
-        { id: 4, name: 'Arm cuffs' },
+    { id: 4, name: 'Nose studs' },
+    { id: 4, name: 'Toe rings' },
+    { id: 4, name: 'Arm cuffs' },
 
-      
-      ]);
-      const [showBrandList, setShowBrandList] = useState(false);
-      const [selectedBrand, setSelectedBrand] = useState(null);
-      
+
+  ]);
+  const [showBrandList, setShowBrandList] = useState(false);
+  const [selectedBrand, setSelectedBrand] = useState(null);
+
   function handleBrandSelect(brand) {
     setSelectedBrand(brand);
     setShowBrandList(false);
     // do something with the selected brand, such as filtering a list of products
-//     const iconContainer = document.querySelector('.iconFlech');
+    //     const iconContainer = document.querySelector('.iconFlech');
 
-//     iconContainer.addEventListener('click', function() {
-//     iconContainer.classList.toggle('clicked');
-// });
+    //     iconContainer.addEventListener('click', function() {
+    //     iconContainer.classList.toggle('clicked');
+    // });
 
   }
   const [isRotated, setIsRotated] = useState(false);
 
   function handleClick() {
     setIsRotated(!isRotated);
-    
+
   }
 
   function handleclick1() {
@@ -56,7 +58,7 @@ function Categories(){
     );
 
     const queryParams = new URLSearchParams();
-    
+
     queryParams.append('tab', JSON.stringify(filteredProducts));
 
     navigate(`/shop?${queryParams.toString()}`);
@@ -91,47 +93,47 @@ function Categories(){
 
     navigate(`/shop?${queryParams.toString()}`);
   }
-  function handleClear(){
+  function handleClear() {
     const queryParams = new URLSearchParams();
     queryParams.append('tab', JSON.stringify(products));
-    navigate(`/shop?${queryParams.toString()}`);  }
+    navigate(`/shop?${queryParams.toString()}`);
+  }
   return (
     <div className={styles.containerCategoriesHr}>
-      
-      <div className={styles.containerCategories}>
-        <div>
+       <div className={styles.containerCategories}>
+         {/* <div>
           <div className={styles.hh}>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</div>
-          
-           </div>
-          <div className={`${styles.containerBrandList} ${styles.elemItem} ${styles.elem1}`}>
-           
-           {/* <button  className={styles.btnAllBrands} onClick={() => setShowBrandList(!showBrandList)}>ALL BRANDS  <img   src={iconfleche} className={styles.isRotated ? 'iconFlech clicked' : 'iconFlech'} 
+
+        </div>
+        <div className={`${styles.containerBrandList} ${styles.elemItem} ${styles.elem1}`}>
+
+           <button  className={styles.btnAllBrands} onClick={() => setShowBrandList(!showBrandList)}>ALL BRANDS  <img   src={iconfleche} className={styles.isRotated ? 'iconFlech clicked' : 'iconFlech'} 
       onClick={handleClick}/></button>
            
            
            <div  className={styles.theList} >
            {showBrandList && <BrandList brands={brands} onBrandSelect={handleBrandSelect} />}
            
-           </div> */}
-           
-           
-           {/* {selectedBrand && <p>You selected {selectedBrand.name}</p>} */}
-       </div>
-       <div className={`${styles.elemItem} ${styles.elem2}`} onClick={handleclick1}>Bracelet</div>
-       <div className={`${styles.elemItem} ${styles.elem2}`} onClick={handleclick2}>Collier</div>
-       <div className={`${styles.elemItem} ${styles.elem3}`} onClick={handleclick3}>Bague</div>
-       <div className={`${styles.elemItem} ${styles.elem4}`} onClick={handleclick4}> Boucles oreilles</div>
-       <div className={`${styles.elemItem} ${styles.elem4}`} onClick={handleClear}>clear</div>
+           </div> 
 
-       
-       
-       
-        </div>
-              <hr ></hr>
+
+           {selectedBrand && <p>You selected {selectedBrand.name}</p>} 
+        </div>  */}
+        <div className={`${styles.elemItem} ${styles.elem2}`} onClick={handleclick1}>Bracelet</div>
+        <div className={`${styles.elemItem} ${styles.elem2}`} onClick={handleclick2}>Collier</div>
+        <div className={`${styles.elemItem} ${styles.elem3}`} onClick={handleclick3}>Bague</div>
+        <div className={`${styles.elemItem} ${styles.elem4}`} onClick={handleclick4}> Boucles oreilles</div>
+        <div className={`${styles.elemItem} ${styles.elem4}`} onClick={handleClear}><FontAwesomeIcon icon={faXmark} className={styles.icon}/></div>
+
+
+
+
+      </div>
+      <hr ></hr> 
 
     </div>
-    
+
   );
-             
+
 }
 export default Categories;
